5610b448a2196d8544bb3fab6ca882056eeb0784
linhuiw <solesnip@gmail.com>
2018-01-22 21:42:29 +0800

    Fix ng demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,34 @@
 import { Component } from '@angular/core';
 const data = [
-  { country: 'Europe', year: '1750', value: 163 },
-  { country: 'Europe', year: '1800', value: 203 },
-  { country: 'Europe', year: '1850', value: 276 },
-  { country: 'Europe', year: '1900', value: 408 },
-  { country: 'Europe', year: '1950', value: 547 },
-  { country: 'Europe', year: '1999', value: 729 },
-  { country: 'Europe', year: '2050', value: 628 },
-  { country: 'Europe', year: '2100', value: 828 },
-  { country: 'Asia', year: '1750', value: 502 },
-  { country: 'Asia', year: '1800', value: 635 },
-  { country: 'Asia', year: '1850', value: 809 },
-  { country: 'Asia', year: '1900', value: 947 },
-  { country: 'Asia', year: '1950', value: 1402 },
-  { country: 'Asia', year: '1999', value: 3634 },
-  { country: 'Asia', year: '2050', value: 5268 },
-  { country: 'Asia', year: '2100', value: 7268 }
+  { year: '1991', value: 3 },
+  { year: '1992', value: 4 },
+  { year: '1993', value: 3.5 },
+  { year: '1994', value: 5 },
+  { year: '1995', value: 4.9 },
+  { year: '1996', value: 6 },
+  { year: '1997', value: 7 },
+  { year: '1998', value: 9 },
+  { year: '1999', value: 13 },
 ];
 
-const dataPre = {
-  transform: [{
-    type: 'percent',
-    field: 'value',
-    dimension: 'country',
-    groupBy: ['year'],
-    as: 'percent'
-  }]
-};
 const scale = [{
-  dataKey: 'percent',
+  dataKey: 'value',
   min: 0,
-  formatter: '.2%',
+}, {
+  dataKey: 'year',
+  min: 0,
+  max: 1,
 }];
 
 @Component({
   selector: 'app-root',
   template: `
   <div>
-    <v-chart [forceFit]="forceFit" [height]="height" [data]="data" [dataPre]="dataPre" [scale]="scale">
+    <v-chart [forceFit]="forceFit" [height]="height" [data]="data" [scale]="scale">
       <v-tooltip></v-tooltip>
       <v-axis></v-axis>
-      <v-stack-bar position='year*percent' color='country' [style]="{ stroke: '#fff', lineWidth: 1 }" ></v-stack-bar>
+      <v-line position="year*value"></v-line>
+      <v-point position="year*value" shape="circle"></v-point>
     </v-chart>
   </div>
   `
@@ -50,7 +38,6 @@ export class AppComponent {
   forceFit = true;
   height = 400;
   data = data;
-  dataPre = dataPre;
   scale = scale;
   fields = ['cut', 'clarity'];
 }
