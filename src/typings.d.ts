8ca4d89cdef79ade4c389ea2767d57daaccc4218
linhuiw <solesnip@gmail.com>
2018-01-22 21:40:57 +0800

    Fix update types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,14 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+interface NodeRequireFunction {
+  (id: string): any;
+}
+interface NodeRequire extends NodeRequireFunction {
+  resolve(id: string): string;
+  cache: any;
+  main: NodeModule | undefined;
+}
+declare var require: NodeRequire;
 interface NodeModule {
   id: string;
 }
